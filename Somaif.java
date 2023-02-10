ccf705283836d55290ca53d7d877c4f29baee9b5
Flávia Almeida <123841045+Flahaga@users.noreply.github.com>
2023-02-09 23:44:09 -0300

    Add files via upload

---
+++ Somaif.java
@@ -0,0 +1,42 @@
+package Laços;
+
+import java.util.Scanner;
+
+public class Somaif {
+
+	public static void main(String[] args) {
+		 int A,B,C,Soma; // A-2 B-4 C-5
+			
+		 Scanner  Leia  = new Scanner(System.in);
+			
+	       System.out.println("\n Coloque o valor de A :");
+		     A= Leia.nextInt();
+		     
+		     System.out.println("\n Coloque o valor de B :");
+		     B = Leia.nextInt();
+		     
+		     System.out.println("\n Coloque o valor de C :");
+		     C = Leia.nextInt();
+		    
+		     if (A+B>C); {
+		     if(Soma>5 && Soma<5) {
+	    	   System.out.println("\n O valor da soma de a+b é maior que c :");
+	       }
+	       
+	        if (A+B<C); {
+	        	if(Soma>5 && Soma<5) { 
+	        	System.out.println("\n O valor da soma de a+b é menor que c :");
+	        }
+	        	
+	        	if (Soma>=5 && Soma<=5) { 
+	         if (A+B==C); {
+		System.out.println("\n O valor da soma de a+b é igual a c :");
+	         }	        	        	
+
+	        		         
+	        		        	
+		             	        	        	        	   		         
+	
+	}	}		
+	
+		     	        	        
