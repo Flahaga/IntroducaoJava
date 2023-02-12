158694768ed884ad0a0e3ec659259286b455a978
Flávia Almeida <123841045+Flahaga@users.noreply.github.com>
2023-02-12 17:13:45 -0300

    Add files via upload

---
+++ SaqueSwicht.java
@@ -0,0 +1,28 @@
+package Laços;
+
+import java.util.Scanner;
+
+public class SaqueSwicht {
+
+	public static void main(String[] args) {
+		
+		int saque,saldo,déposito,operação,novosaldo;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("\n Digite o valor do saque :");
+		saque = leia.nextInt();
+		System.out.println("\n Digite o valor do seu déposito :");
+		déposito = leia.nextInt();
+		
+		System.out.println("\n Operações :");
+		System.out.println("\n Código 1 - Saldo :");
+		System.out.println("\n Código 2 - Saque :");
+		System.out.println("\n Código 3 - Déposito :");
+		
+		
+		
+
+	}
+
+}
