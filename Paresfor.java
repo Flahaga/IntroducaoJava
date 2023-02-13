c716c52120e352acd0f7a1f744f3045d598ffda2
Flávia Almeida <123841045+Flahaga@users.noreply.github.com>
2023-02-12 22:25:49 -0300

    Add files via upload

---
+++ Paresfor.java
@@ -0,0 +1,47 @@
+package Laçosrepetição;
+
+import java.util.Scanner;
+
+public class Paresfor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int  num,x,Par=0,Ímpar = 0;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		for (x=1;x<=10;x++){
+		 {
+			 System.out.println("\n Digite um número :");
+				num = leia.nextInt();
+			
+				if(num % 2 == 0) {
+				Par = (Par+1);
+				if(num>0) {
+				}		System.out.println("\n Informe o número de pares: " +Par);
+				}
+				      Ímpar = (Ímpar-(-1));
+					if(num>0) {
+						System.out.println("\n Informe o número de ímpares é :"  +Ímpar);
+						
+
+					
+				}	 
+							
+				}   
+			}
+				}
+
+	
+	}			
+			
+			  			
+			  
+			  
+			  
+			  
+			  
+			  
+			  
+			 
