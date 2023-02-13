96039c6e2de445b615108234bf7785396c126be7
Flávia Almeida <123841045+Flahaga@users.noreply.github.com>
2023-02-12 22:18:57 -0300

    Add files via upload

---
+++ Whiledo.java
@@ -0,0 +1,35 @@
+package Laçosrepetição;
+
+import java.util.Scanner;
+
+public class Whiledo {
+
+	public static void main(String[] args) {
+		
+		
+        int x,num,somap = 0,contagem = 0,soma = 0; 
+        
+        Scanner leia = new Scanner(System.in);
+       
+        for (x=1;x<=10;x++){
+        }
+        System.out.println( "Digite um número" );
+          num = leia.nextInt();
+            
+	       if (num >= 0 ){ 
+	        	 somap = (num + soma);
+	        	 contagem ++;
+            }      System.out.println("\n A soma dos números positivos é :" +somap);
+	       { while ( num >= 0 ); 
+                 }  
+         System.out.println ( "A quantidade de números digitados foi " + contagem );
+                 {            
+              System.out.println ( "A média é " + soma / contagem );         
+}
+		{	
+		
+		}		
+	}			
+}               
+	          	          
+				
